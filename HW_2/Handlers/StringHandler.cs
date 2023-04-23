d8322253d5beb6c788698451bf7196e71064c062
yuliaBiriukova <yuliia.biriukova@nure.ua>
2023-04-24 02:02:21 +0300

    Implement Task 1

---
+++ HW_2/Handlers/StringHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace HW_2.Handlers
+{
+    internal class StringHandler
+    {
+        // return sum of digits in string 
+        public static int SumDigitsInString(string? str)
+        {
+            if (string.IsNullOrEmpty(str) || !str.Any(char.IsDigit))
+                return 0;
+
+            return Convert.ToInt32(str.Where(char.IsDigit).Sum(char.GetNumericValue));
+        }
+
+        // return maximum digit in string
+        public static int GetMaxDigitInString(string? str)
+        {
+            if (string.IsNullOrEmpty(str) || !str.Any(char.IsDigit))
+                return 0;
+
+            return Convert.ToInt32(str.Where(char.IsDigit).Select(char.GetNumericValue).Max());
+        }
+
+    }
+}
